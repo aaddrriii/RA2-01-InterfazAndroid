e33367b5152a5e3b15275babb5717337ec78dad2
aaddrriii <adriansalvadorperez2@gmail.com>
2026-09-23 16:54:34 +0200

    Initial commit - Proyecto base Android 13 API 33 Jetpack Compose

---
+++ app/src/main/java/com/example/ra2_01_interfazandroid/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.ra2_01_interfazandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ra2_01_interfazandroid.ui.theme.RA201InterfazAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RA201InterfazAndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RA201InterfazAndroidTheme {
+        Greeting("Android")
+    }
+}
